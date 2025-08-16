400f7a30decb0eaf2f174e5ca0c8e99b20f00c7a
nagarajRPoojari <np137270@gmail.com>
2025-08-16 10:18:34 +0530

    fix: disable log

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "github.com/nagarajRPoojari/orangectl/internal/proxy"
+import (
+	"github.com/nagarajRPoojari/orangectl/internal/proxy"
+	log "github.com/nagarajRPoojari/orangectl/internal/utils/logger"
+)
 
 // orangectl should only be used within a k8s environment in prod mode
 // since it is currently tightly coupled with k8s environment
 func main() {
+	log.Disable()
+
 	p := proxy.NewProxy(":8000")
 	p.WatchShards()
 	p.Serve()
